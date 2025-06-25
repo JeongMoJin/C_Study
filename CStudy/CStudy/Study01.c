121705336df4727e9dad28ccb7e5f2784b5d6ada
LearnAndRecord <wlswjdah123@naver.com>
2025-06-26 00:48:53 +0900

    20250625

--- CStudy/CStudy/Study01.c
+++ CStudy/CStudy/Study01.c
@@ -3,6 +3,8 @@ int main()
 {
 	int arr[10] = { 2, 10, 30, 21, 34, 23, 53, 21, 9, 1 };
 	int i;
+	int a; 
+	int iGoKrafton;
 	for (i = 0; i < 10; i++)
 	{
 		printf("배열의 %d 번째 원소 : %d \n", i + 1, arr[i]);
